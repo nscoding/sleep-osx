003ccb40dfef6795a696446ad9d1e1c459b36594
Patrik Tomas Chamelo <nscoding@nscoding-mbp.local>
2014-11-09 20:17:52 -0800

    CHANGE: Moved to pmset with NSTask

--- Sleep/Sleep/AppDelegate.swift
+++ Sleep/Sleep/AppDelegate.swift
@@ -29,10 +29,10 @@ class AppDelegate: NSObject, NSApplicationDelegate
 {
     func applicationDidFinishLaunching(aNotification: NSNotification?)
     {
-        var scriptObject : NSAppleScript = NSAppleScript(source: "tell application \"System Events\" to sleep")!
-        var errorDict: NSDictionary? = nil
-        scriptObject.executeAndReturnError(&errorDict)
-    
+        var task = NSTask()
+        task.launchPath = "/usr/bin/pmset"
+        task.arguments = ["sleepnow"]
+        task.launch()
         exit(0)
     }
 }
